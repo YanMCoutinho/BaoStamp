47c55401d0dfe772cee941f86d94ed1157e5f452
YanMCoutinho <yan.m.coutinho@gmail.com>
2024-06-08 20:44:44 -0300

    finish added product

--- cartesi-backend/dapp.py
+++ cartesi-backend/dapp.py
@@ -1,7 +1,12 @@
 from os import environ
-import logging
 import requests
+import logging
+import traceback
+import json
 
+"""
+BASIC STRUCTURE OF THE DAPP
+"""
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
 
@@ -21,8 +26,109 @@ def str2hex(str):
     """
     return "0x" + str.encode("utf-8").hex()
 
+def send_notice(notice: str) -> None:
+    send_post("notice", notice)
+
+
+def send_report(report: str) -> None:
+    send_post("report", report)
+
+def send_post(endpoint, json_data) -> None:
+    response = requests.post(rollup_server + f"/{endpoint}", json=json_data)
+    logger.info(
+        f"/{endpoint}: Received response status {response.status_code} body {response.content}")
+
+"""
+CUSTOM FUNCTIONS
+"""
+
+def classify(input):
+    return 0
+
+"""
+Expected JSON
+payload
+    '
+{
+    "type": 0,
+    "data": {
+    "product_name": "name",
+    "steps": [
+        {
+        "step": 1,
+        "process": "process_name",
+        "inputs": {
+            "item_name": "qtd"
+        },
+        "output": {
+            "item_name": "qtd"
+        },
+        "date_init": "2023-01-01",
+        "date_end": "2023-07-01",
+        "desc": ""
+        },
+        {
+        "step": 1,
+        "process": "process_name",
+        "inputs": {
+            "item_name": "qtd"
+        },
+        "output": {
+            "item_name": "qtd"
+        },
+        "date_init": "2023-01-01",
+        "date_end": "2023-07-01",
+        "desc": ""
+        }
+    ],
+    "components": [
+        {
+        "name": "Corante Anilina",
+        "step": 6,
+        "desc": "paia"
+        }
+    ]
+    }
+}
+'
+
+JSON descriptions
+type
+0 = insert product
+1 = insert production
+2 = insert new sku (product)
+
+"""
+
+products = {}
+
+def insert_product(input):
+    msg_sender = input['msg_sender']
+    if products.get(msg_sender, 0) == 0:
+        products[msg_sender] = []
+    products[msg_sender].append(input['data'])
+    logger.info(f"Array {products[msg_sender]}")
+
+#routes by type
+routes = {
+    0: insert_product
+}
 def handle_advance(data):
     logger.info(f"Received advance request data {data}")
+    try:
+        input = hex2str(data["payload"])
+        input_json = json.loads(format(input))
+        print(type(input_json))
+        input_json['msg_sender'] = data['metadata']['msg_sender']
+        handler_function = routes[input_json['type']]
+        handler_function(input_json)        
+
+    except Exception as e:
+        msg = f"Error {e} processing data {data}"
+        logger.error(f"{msg}\n{traceback.format_exc()}")
+        send_report({"payload": str2hex(msg)})
+        return "reject"
+    
     return "accept"
 
 
@@ -31,6 +137,9 @@ def handle_inspect(data):
     return "accept"
 
 
+"""
+Other dapp functions
+"""
 handlers = {
     "advance_state": handle_advance,
     "inspect_state": handle_inspect,
