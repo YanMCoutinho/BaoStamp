f265f8d9c6facf89a1b1e11ba96f6f05596209cd
YanMCoutinho <yan.m.coutinho@gmail.com>
2024-06-08 13:28:14 -0300

    Added string decodifier

--- cartesi-backend/dapp.py
+++ cartesi-backend/dapp.py
@@ -8,6 +8,18 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
+def hex2str(hex):
+    """
+    Decodes a hex string into a regular string
+    """
+    return bytes.fromhex(hex[2:]).decode("utf-8")
+
+
+def str2hex(str):
+    """
+    Encodes a string as a hex string
+    """
+    return "0x" + str.encode("utf-8").hex()
 
 def handle_advance(data):
     logger.info(f"Received advance request data {data}")
